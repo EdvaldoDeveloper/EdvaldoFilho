4cb648648428858f4f5169363083b675f34790d8
isplatform <iscode2018@gmail.com>
2021-06-20 00:36:14 -0300

    writing adjustments

--- start/routes.js
+++ start/routes.js
@@ -3,7 +3,9 @@ const routes = new Router();
 
 const Home = require("../app/controllers/Home");
 
+
 routes.get("/", Home.index)
 routes.post("/savesMessage", Home.story)
+
 
 module.exports = routes;
